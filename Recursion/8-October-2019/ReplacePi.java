ed41b5b9c195cd56e9746ddb57bb664b56f97031
Technobug0877 <technoxpert12@gmail.com>
2019-10-08 18:01:27 +0530

    Replace Pi

---
+++ Recursion/8-October-2019/ReplacePi.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class solution {
+
+    static String ch = "";
+    static int i = 0;
+	public static String replace(String input){
+      
+            return input.replace("pi","3.14");          
+	}
+}
+
+public class runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String input = s.nextLine();
+		System.out.println(solution.replace(input));
+	}
+}
