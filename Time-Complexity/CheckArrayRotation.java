a0fbbdd678ccc3c7b9240ef60737dbf606f8969e
Technobug0877 <technoxpert12@gmail.com>
2019-10-17 23:24:52 +0530

    Check Array Rotation

---
+++ Time-Complexity/CheckArrayRotation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class PairSum{	
+
+	public static void pairSum(int[] arr, int num){
+	
+      	Arrays.sort(arr);
+      	for(int i=0;i<arr.length;i++){
+          for(int j=i+1;j<arr.length;j++){
+            if(arr[i]+arr[j]==num){
+              System.out.println(arr[i]+" "+arr[j]);
+            }
+          }
+        }
+	}
+}
+
+public class Main {
+	
+	public static int[] takeInput() {
+		Scanner s = new Scanner(System.in);
+		int size = s.nextInt();
+		int arr[] = new int[size];
+		for (int i = 0; i < size; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = takeInput();
+		System.out.print(CheckRotation.arrayRotateCheck(arr));
+	}
+}
